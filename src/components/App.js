aa72ea9066181dbbb87a3dce60767b70da69481a
MuhammadYousuf20 <39589913+MuhammadYousufHere@users.noreply.github.com>
2022-04-12 13:09:46 +0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Test from "./Test/Test";
+function App() {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Test/Test.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Test.scss";
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React Starter</h1>
+        <p>Let's Build Something Fun!</p>
+      </div>
+    );
+  }
+}
+export default Test;
